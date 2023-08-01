6608b3c33d92555a4659920dc3679194fc581702
chiadi <lcim1104@gmail.com>
2023-08-01 13:57:23 +0100

    added instructions

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -2,6 +2,14 @@ import NewCard from "../NewCards/NewCard";
 import "./searchResults.css";
 
 const SearchBox = ({ suggestedNames, keyWord }) => {
+
+  const instructions = (
+    <p className="instructions">
+      When you type your keyword, matching names will show here; click on any of
+      it to open the page on a new tab.
+    </p>
+  );
+
   const content = suggestedNames.map((result) => {
     return <NewCard key={result} result={result} />;
   });
@@ -11,6 +19,7 @@ const SearchBox = ({ suggestedNames, keyWord }) => {
         keyWord.length > 0 ? "with-background" : "without-background"
       }`}
     >
+      {keyWord.length === 0 && instructions}
       {content.slice(0, 20)}
     </div>
   );
