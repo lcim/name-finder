442837f4120cafbb87cef6c58e93196bab86d4f2
chiadi <lcim1104@gmail.com>
2023-08-02 21:00:56 +0100

    Delayed userInput feedback using debounce

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,7 @@ import SearchBox from "../SearchBox/SearchBox";
 import SearchResults from "../SearchResults/SearchResults";
 import name from "@rstacruz/startup-name-generator";
 
+// I used functional component using useState hook in place of class in version 2 (see bottom of the page). This is why I was asking questions about version control.
 class App extends React.Component {
   state = {
     headerText: "Domain Search",
@@ -37,3 +38,36 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+// Using functional component for the same App
+
+/* 
+export default function App() {
+
+  const[headerText, setHeaderText] = useState('Domain Searcher')
+  const[keyWord, setKeyWord] = useState("")
+  const[suggestedNames, setSuggestedNames] = useState([])
+  
+  const handleKeyWord = (inputText) => {
+    setHeaderText("Domain Searcher")
+    setKeyWord(inputText)
+    setSuggestedNames ( inputText ? name(inputText) : []
+    );
+  };
+
+    return (
+      <div className="App">
+        <Header
+          headerText={headerText}
+          keyWord={keyWord}
+        />
+        <SearchBox handleKeyWord={handleKeyWord} />
+        <SearchResults
+          keyWord={keyWord}
+          suggestedNames={suggestedNames}
+        />
+      </div>
+    );
+  }
+*/
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,16 +1,21 @@
 import "./searchBox.css";
+import {debounce} from 'lodash';
 
 const SearchBox = ({ handleKeyWord }) => {
+  
+  const delayedFeedback = debounce((userInput) => {
+    handleKeyWord(userInput)
+  }, 1000)
+
   return (
     <div className="searchBox__container">
       <input
         type="text"
         placeholder="Enter your keyword/name"
-        onChange={(e) => handleKeyWord(e.target.value)}
+        onChange={(e) => delayedFeedback(e.target.value)}
         className="keyword"
       />
     </div>
   );
 };
-
 export default SearchBox;
